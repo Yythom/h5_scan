bc66b476f3ff76e8a4008f04e569447229de0528
yyt <13145216024@163.com>
2020-09-15 10:52:57 +0800

    a

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,34 @@ import BaseLayout from './component/layout/BaseLayout'
 //组件
 import Login from './pages/User/Login'
 import Register from './pages/User/Register'
+import { useEffect } from 'react'
+import VConsole from 'vconsole';
 
 function _App(props) {
+    function IsPC() {
+        var userAgentInfo = navigator.userAgent;
+        console.log(userAgentInfo);
+
+        var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+        var flag = true;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+
+    useEffect(() => {
+
+        console.log(IsPC());
+        if (!IsPC()) {
+            new VConsole()
+        }
+    }, [])
+
     return (
         <>
             <Router>
